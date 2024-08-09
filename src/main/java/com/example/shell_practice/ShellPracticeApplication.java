988b9526a1d94b51df90d1746b383481f46a40ed
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-08-09 14:35:45 +0900

    [Init] 스프링 서버

---
+++ src/main/java/com/example/shell_practice/ShellPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shell_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShellPracticeApplication.class, args);
+    }
+
+}
